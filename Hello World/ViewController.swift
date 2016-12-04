34e60122246763315007a1f47e92ef2a452c6236
prkashi <punithrkashi@gmail.com>
2016-12-03 23:16:37 -0500

    First Commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,14 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var age: UITextField!
+    
+    @IBOutlet var button: UIButton!
+    @IBOutlet var image: UIImageView!
+    
+    @IBAction func submit(_ sender: Any) {
+        
+        print("button tapped")
+
+        label.text = "My age is : " + age.text!
+        
+        age.alpha = 0.00
+        
+        button.alpha = 0.00
+        
+        image.alpha = 1.00
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        image.alpha = 0.00
+        
+    
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func dismissKeyboard(){
+        
+        view.endEditing(true)
+        
     }
 
 
